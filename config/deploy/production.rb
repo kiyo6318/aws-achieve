94fac68618c741a86ceb884ba3e0273f60bd9f02
前川清成 <kiyonari6318@MacbookPro.local>
2019-06-10 16:00:20 +0900

    Fix: deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,3 @@
-server '3.113.50.233', user: 'app', roles: %w{app db web}
+server '3.113.50.233', user: 'app', roles: %w{web}
+server '3.113.50.233', user: 'app', roles: %w{app db}
 set :ssh_options, keys: '/Users/kiyonari6318/.ssh/id_rsa'
